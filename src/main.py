1b9686c7b5d8d664dc5470b9d237703ffc402d41
Ashish Thanki <ashishthanki95@gmail.com>
2023-04-10 22:44:27 +0100

    DS workflow logic added to main.py file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
 """Main module."""
+from data.make_dataset import load_data
 
 
-def main():
-    return
+def main(path: str = "data/raw/data.csv"):
+    # load data
+    raw_data = load_data(path)
+
+    # create train test validation splits
+
+    # transform data
+
+    # explore data - see Jupyter Notebooks in notebooks folder.
+
+    # preprocess data and prepare for modelling
+
+    # build models and shortlist best
+
+    # run hyperparameter optimizer
+
+    # fit model again with best_config
+    
+    # evaluate model
+
+    # visual best features with Synergy
+    return raw_data
+
+
+if __name__ == "__main__":
+    main()
